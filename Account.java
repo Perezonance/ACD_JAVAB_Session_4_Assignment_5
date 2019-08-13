36561674c76a597d17b55d831c5e423b728446df
Perezonance <53836343+Perezonance@users.noreply.github.com>
2019-08-13 00:11:13 -0400

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,38 @@
+package assignment_5;
+
+public class Account {
+	private int accNo;
+	private String accName, accBal;
+	
+	public Account(int accNo, String name, String bal){
+		this.accNo = accNo;
+		this.accName = name;
+		this.accBal = bal;
+	}
+	
+
+	public int getAccNo() {
+		return accNo;
+	}
+
+	public void setAccNo(int accNo) {
+		this.accNo = accNo;
+	}
+
+	public String getAccName() {
+		return accName;
+	}
+
+	public void setAccName(String accName) {
+		this.accName = accName;
+	}
+
+	public String getAccBal() {
+		return accBal;
+	}
+
+	public void setAccBal(String accBal) {
+		this.accBal = accBal;
+	}
+	
+}
